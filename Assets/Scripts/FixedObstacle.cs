4fc201d10e9c4e0c5feae53366f4375ce1409100
Clara Li <clarali2000@126.com>
2015-11-08 02:14:51 -0500

    FINAL OBSTACLE

---
+++ Assets/Scripts/FixedObstacle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FixedObstacle : MonoBehaviour {
+
+	public GameObject planet;
+	float acceleration = 100.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 posDiff = planet.transform.position - this.transform.position;
+		this.GetComponent<Rigidbody2D>().AddForce(posDiff.normalized * acceleration);
+	}
+}
---
+++ Assets/Scripts/GenerateObstacles.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateObstacles : MonoBehaviour {
+
+	public static GameObject[] obstacles;
+	public GameObject ring;
+	public static int numSpawned = 0;
+	public static int numToSpawn = 5;
+	float radius;
+
+	// Use this for initialization
+	void Start () {
+		obstacles = Resources.LoadAll<GameObject>("Prefabs");
+		radius = ring.GetComponent<CircleCollider2D> ().radius;
+	}
+
+	void SpawnRandomObject() 
+	{    
+		//spawns item in array position between 0 and 100
+		int whichItem = Random.Range (0, numToSpawn);
+		GameObject myObj = Instantiate (obstacles [whichItem]) as GameObject;
+		numSpawned++;
+		float theta = Random.Range(-180, 180);
+		float x = radius*Mathf.Cos(theta);
+		float y = radius*Mathf.Sin(theta);
+		//where your instantiated object spawns from
+		myObj.transform.position = new Vector3(x, y, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (numToSpawn > numSpawned) 
+		{
+			SpawnRandomObject ();
+		}
+	}
+}
