69396d8999d0be910a41bb44617c7dbffe7c97c5
Sam Tang <17tangs@my.weston.org>
2015-11-07 23:30:06 -0500

    blah

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -4,21 +4,24 @@ using System.Collections;
 public class PlayerBehavior : MonoBehaviour {
 	public Rigidbody2D rb;
 	public GameObject planet;
-	private float acceleration = 100f;
+	private float acceleration = 200f;
 	private bool jump = false;
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
 		
 	}
-	private int i;
-	void OnCollisionEnter2D(Collision2D obs){
-		if (obs.gameObject.name.Contains("Ring")) {
-			i++;
-			Debug.Log(i);
-			jump = true;
-		}
+	void OnCollisionStay2D(Collision2D obs){
+		jump = true;
+		
 	}
+	void OnCollisionExit2D(Collision2D obs){
+		if(jump == true){
+			//jump = false;
+		}
+
+	}
+	private float nextJump; 
 	// Update is called once per frame
 	void FixedUpdate () {
 		//transform.rotation = Quaternion.Euler (0, 0, Mathf.Atan2(transform.position.y, transform.position.x));
@@ -30,14 +33,14 @@ public class PlayerBehavior : MonoBehaviour {
 		
 		
 		rb.AddForce(posDiff.normalized * acceleration);
-		if (Input.GetKey (KeyCode.Space)) {
-			Vector3 n = Quaternion.Euler(0, 0, -30) * -posDiff;
-			if(jump == true){
-				rb.velocity = n.normalized*20;
-			}
-			//rb.AddForce(n.normalized*20,ForceMode2D.Impulse);
-			//Vector3 op = transform.position;
-			//transform.position = new Vector3(op.x, op.y+0.5f, op.z);
+		if (Input.GetKey (KeyCode.Space) && Time.time>nextJump) {
+				Vector3 n = Quaternion.Euler (0, 0, -30) * -posDiff;
+				rb.velocity = n.normalized * 20;
+				nextJump = Time.time + 10;
+				//rb.AddForce(n.normalized*20,ForceMode2D.Impulse);
+				//Vector3 op = transform.position;
+				//transform.position = new Vector3(op.x, op.y+0.5f, op.z);
+
 		}
 		//rb.AddForce (perpendicular.normalized * 5);
 	}
