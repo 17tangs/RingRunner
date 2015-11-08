36b860059833c5c45ba6aa76a56d17c38cc15091
Sam Tang <17tangs@my.weston.org>
2015-11-08 10:03:14 -0500

    camera improvements

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -7,6 +7,7 @@ public class CameraControl : MonoBehaviour {
 	private Vector3 m_MoveVelocity;                 // Reference velocity for the smooth damping of the position.
 	private Vector3 m_DesiredPosition; 
 	public GameObject planet;
+	public float level;
 	// Use this for initialization
 	void Start () {
 
@@ -15,6 +16,7 @@ public class CameraControl : MonoBehaviour {
 	void FixedUpdate ()
 	{
 		planet = m_Target.GetComponent<PlayerBehavior> ().planet;
+		level = planet.GetComponent<Transform> ().localScale.x*5;
 		Move ();
 	}
 	
@@ -33,7 +35,7 @@ public class CameraControl : MonoBehaviour {
 	private void FindAveragePosition ()
 	{
 		Vector3 posDiff = planet.transform.position - m_Target.transform.position;
-		Vector3 averagePos = -posDiff.normalized * 7;
+		Vector3 averagePos = -posDiff.normalized * level;
 		//Vector3 averagePos = m_Target.transform.position;
 		averagePos.z = transform.position.z;
 		// The desired position is the average position;
