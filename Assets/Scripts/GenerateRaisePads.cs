757acd3fec4c945b43e116fc4c296da640e8b666
nimsarr <simon.bass81@gmail.com>
2015-11-08 04:57:48 -0500

    ring prefab + generate raise pads beginnings

---
+++ Assets/Scripts/GenerateRaisePads.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Math;
+
+public class GenerateRaisePads : MonoBehaviour {
+
+	public static GameObject[] raisePads;
+	public GameObject ring;
+	public static int numSpawned = 0;
+	public static int numToSpawn = 8;
+	float radius;
+	
+	// Use this for initialization
+	void Start () {
+		radius = (ring.GetComponent<CircleCollider2D> ().radius) * (ring.GetComponent<Transform>().localScale.x);
+	}
+	
+	void SpawnRandomObject() 
+	{    
+		//spawns item in array position between 0 and numToSpawn
+		int whichItem = Random.Range (0, numToSpawn);
+		GameObject myObj = Instantiate (raisePads [whichItem]) as GameObject;
+		numSpawned++;
+		float theta = Random.Range(-180, 180);
+		float x = radius*Mathf.Cos(theta);
+		float y = radius*Mathf.Sin(theta);
+		float y = radius*Mathf.Tan(theta);
+		//where your instantiated object spawns from
+		myObj.transform.position = new Vector3(x, y, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (numToSpawn > numSpawned) 
+		{
+			SpawnRandomObject ();
+		}
+	}
+
+	float toDegrees(float rad){
+		return rad * 180 / Math.PI;
+	}
+}
