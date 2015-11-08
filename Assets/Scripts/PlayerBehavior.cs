bcd5ccb06433d058bbdbe1e48a73d650fb6c0059
Sam Tang <17tangs@my.weston.org>
2015-11-08 04:52:27 -0500

    :((((((((((((((((((((((((((

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -4,50 +4,106 @@ using System.Collections;
 public class PlayerBehavior : MonoBehaviour {
 	public Rigidbody2D rb;
 	public GameObject planet;
+	private Vector3 d;
 	private float acceleration = 150f;
-	private bool jump = false;
+	private bool jump = true;
+	private Vector3 desired;
+	private bool reach = true;
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
-		
+		desired = transform.position;
 	}
-	void OnCollisionStay2D(Collision2D obs){
+/*	void OnCollisionStay2D(Collision2D obs){
 		jump = true;
 		
 	}
 	void OnCollisionExit2D(Collision2D obs){
-		if(jump == true){
+		jump = false;
+
+
+	}*/
+
+	private float nextJump; 
+	private int i;
+	/*void Update(){
+		float magnitude = transform.localScale.x * GetComponent<CircleCollider2D> ().radius + 
+			planet.transform.localScale.x * planet.GetComponent<CircleCollider2D> ().radius;
+		Vector3 posDiff = planet.transform.position - transform.position;
+		bool touching = Mathf.Abs (posDiff.magnitude - magnitude) < 0.1;
+		if (touching) {
+			jump = true;
+			reach = false;
+			//desired = transform.position;
+		} else {
 			jump = false;
 		}
 
-	}
-	void Update(){
-	}
-	private float nextJump; 
-	private int i;
-	// Update is called once per frame
+		if (Input.GetAxis ("Jump")==1){
+			if(jump == true){
+				desired = (transform.position - posDiff)*2f;
+				Debug.Log (desired);
+				//move ();
+			}
+			
+		}
+		//	Vector3 desired = transform.position;
+
+		if ((transform.position - desired).magnitude < 0.1f) {
+			reach = true;
+		}
+		if(reach == false){
+			move ();
+		}
+	}*/
+		// Update is called once per frame
 	void FixedUpdate () {
 		Vector3 posDiff = planet.transform.position - transform.position;
-		Vector3 perpendicular = new Vector3 (-posDiff.y, posDiff.x, posDiff.z);
+
+		float magnitude = transform.localScale.x * GetComponent<CircleCollider2D> ().radius + 
+			planet.transform.localScale.x * planet.GetComponent<CircleCollider2D> ().radius;
+		bool touching = Mathf.Abs (posDiff.magnitude - magnitude) < 0.1;
+		if (touching) {
+			jump = true;
+		} else {
+			jump = false;
+		}
+		Vector3 perpendicular = Quaternion.Euler(0,0,0)* new Vector3 (-posDiff.y, posDiff.x, posDiff.z);
 		rb.velocity = new Vector2(perpendicular.x, perpendicular.y).normalized * 5;
+		//if (jump == false) {rb.AddForce (posDiff.normalized * acceleration);}
 		rb.AddForce(posDiff.normalized * acceleration);
+		//desired = transform.position;
+	
 		Vector3 n = Quaternion.Euler (0, 0, -30) * -posDiff;
-		Debug.Log (Input.GetKey (KeyCode.Space));
-		if (Input.GetKeyDown (KeyCode.Space)){
+		if (Input.GetAxis ("Jump")==1){
 			if(jump == true){
-				move ();
+				//desired = (transform.position - posDiff)*0.7f;
+				Debug.Log ("new: " + new Vector2(n.x,n.y));
+				rb.velocity = new Vector2(n.x, n.y) * 10;
+
+				//move ();
 			}
 
 			
 		}
-		//rb.AddForce (perpendicular.normalized * 5);
+		Debug.Log (rb.velocity);
+		//if (jump == true) {
+		//	transform.position = Vector3.Lerp (transform.position, desired, 5);
+		//}
+
+		//rb.AddForce (perpendicular.normalized * 5);*/
 	}
 	void move(){
 		Vector3 posDiff = planet.transform.position - transform.position;
-		Vector3 n = -posDiff;//Quaternion.Euler (0, 0, -30) * -posDiff;
-//		rb.AddForce (n*10);
-		rb.velocity += new Vector2(n.x, n.y) * 10;
 
+
+		//transform.position = Vector3.Lerp (transform.position, desired, Time.deltaTime*5);
+
+		//Vector3 posDiff = planet.transform.position - transform.position;
+		Vector3 n = Quaternion.Euler (0, 0, 0) * -posDiff;
+		//transform.position = Vector3.Lerp(transform.position, (transform.position - posDiff)*0.6f, 30);
+		rb.velocity += new Vector2(n.x, n.y) * 10;
+		//rb.AddForce(new Vector2(n.x,n.y)*100);
 
 	}
 }
