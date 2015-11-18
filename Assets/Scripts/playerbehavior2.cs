2e78b97f714453cbd4215985741309f1e0852247
Sam Tang <17tangs@my.weston.org>
2015-11-18 16:09:54 -0500

    thing

--- Assets/Scripts/playerbehavior2.cs
+++ Assets/Scripts/playerbehavior2.cs
@@ -2,66 +2,48 @@
 using System.Collections;
 
 public class playerbehavior2 : MonoBehaviour {
-	public Rigidbody2D rb;
 	public GameObject planet;
-	private Vector3 d;
-	private float acceleration = 150f;
-	private bool jump = true;
-	private Vector3 desired;
-	private bool reach = true;
 	private float radius;
-	private float theta = 0;
-	// Use this for initialization
+	public float theta = 0;
+	private int current = 4;
+	[Range(0.00f, 0.05f)]
+	public float AngleIncrement = 0.02f ;
 	void Start () {
-		rb = GetComponent<Rigidbody2D> ();
 		planet = GameObject.Find ("Ring4");
 		Debug.Log (planet);
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey ("1")) {
-			planet = GameObject.Find ("Ring1");
+		planet = GameObject.Find ("Ring"+ current.ToString());
+		if (Input.GetKeyDown (KeyCode.UpArrow)) 
+		{
+			if (current < 4) 
+			{
+				current++;
+			}
+		} 
+		else if (Input.GetKeyDown (KeyCode.DownArrow)) 
+		{
+			if (current > 1) 
+			{
+				current--;
+			}
 		}
-		else if(Input.GetKey ("2")) {
-			planet = GameObject.Find ("Ring2");
-		}
-		else if(Input.GetKey ("3")) {
-			planet = GameObject.Find ("Ring3");
-		}
-		else if(Input.GetKey ("4")) {
-			planet = GameObject.Find ("Ring4");
-		}
+
 	}
 
 	void FixedUpdate () {
-		//move ();
+		Debug.Log (theta);
 		Vector3 posDiff = planet.transform.position - transform.position;
-		//rb.AddForce(posDiff.normalized * acceleration);
 		if (theta > 180f) {
 			theta = -180f;
 		}
-		theta += 0.02f;
-		//planet = PlayerBehaviorRings.currentRing;
+		theta += AngleIncrement;
 		radius = GameObject.Find("Player").transform.localScale.x * GameObject.Find ("Player").GetComponent<CircleCollider2D> ().radius + 
 			planet.transform.localScale.x * planet.GetComponent<CircleCollider2D> ().radius;
-		//Debug.Log (radius);
 		float x = radius * Mathf.Cos (theta);
 		float y = radius * Mathf.Sin (theta);
-		//Debug.Log (new Vector2(x,y));
-		//float ang = radius*Mathf.Tan(toRad(theta));
-		//where your instantiated object spawns from
 		transform.position = new Vector3 (x, y, 0);
 	}
-	void move(){
-		Vector3 posDiff = planet.transform.position - transform.position;
-		if (Input.GetAxis ("Jump") == 1) {
-			//if (jump == true) {
-				Vector3 n = Quaternion.Euler (0, 0, 0) * -posDiff;
-				rb.velocity += new Vector2(n.x, n.y) * 2f;
-				//newpos = -posDiff.normalized*height;// (transform.position - posDiff)*2f;
-			//	jump = false;
-			//}
-		}
-	}
+
 }
